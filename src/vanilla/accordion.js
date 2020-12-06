716aa58cf5674a11b73a119a00cb318fecdbf4d0
Marcelo Reis <oi@marcelreis.dev>
2020-12-06 18:33:17 -0300

    feat: ajustes do accordion

---
+++ src/vanilla/accordion.js
@@ -0,0 +1,20 @@
+// @ts-check javascript
+
+const accordion = document.querySelector(".accordion");
+const accodionItems = document.querySelectorAll(".accordion__item");
+
+let activeItem = undefined;
+
+accodionItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("accordion__item--active");
+
+    if (activeItem !== item) {
+      activeItem?.classList.toggle("accordion__item--active");
+      activeItem = item;
+      return;
+    }
+
+    activeItem = undefined;
+  });
+});
--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,5 +1,6 @@
 import "normalize.css";
 import "../styles/index.scss";
 
+import "./accordion";
 import "./carousel";
 import "./slider";
