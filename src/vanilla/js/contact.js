c71ddb09d7d4f9dc2da00c83ce9778a7e141866b
Marcelo Reis <oi@marcelreis.dev>
2020-12-07 01:20:58 -0300

    feat: validação de form no vanilla

---
+++ src/vanilla/js/contact.js
@@ -0,0 +1,40 @@
+import {
+  validateEmail,
+  validateMessage,
+  validateName,
+  validatePhone,
+} from "../../common/utils/validator";
+
+const contactForm = document.querySelector(".contact");
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
+
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!validateName(name.nodeValue)) {
+    addInputError(name);
+  }
+  if (!validateEmail(email.nodeValue)) {
+    addInputError(email);
+  }
+  if (!validatePhone(phone.nodeValue)) {
+    addInputError(phone);
+  }
+  if (!validateMessage(message.nodeValue)) {
+    addInputError(message);
+  }
+
+  if (contactForm.querySelectorAll(".contact__textField--error").length) {
+    alert("Favor preencher os campos corretamente");
+  }
+});
+
+function addInputError(inputElement) {
+  inputElement.parentElement.classList.add("contact__textField--error");
+}
