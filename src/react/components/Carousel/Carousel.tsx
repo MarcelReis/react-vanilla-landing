e8a120cf9bc7eb8d78c8d0084c191e8bde27c01c
Marcelo Reis <oi@marcelreis.dev>
2020-12-06 23:06:06 -0300

    feat: melhorias de carousel react  - adiciona cursor ponter nos botões radios

--- src/react/components/Carousel/Carousel.tsx
+++ src/react/components/Carousel/Carousel.tsx
@@ -1,41 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+const images = [
+  { src: "https://placekitten.com/321/210", alt: "" },
+  { src: "https://placekitten.com/321/210", alt: "" },
+  { src: "https://placekitten.com/321/210", alt: "" },
+];
 
 const Carousel = () => {
+  const [activeItem, setactiveItem] = useState(0);
+
   return (
     <div className="carousel">
-      <div className="carousel__container">
-        <div className="carousel__imageContainer">
-          <img
-            className="carousel__image"
-            src="https://placekitten.com/321/210"
-            alt=""
-          />
-        </div>
-        <div className="carousel__imageContainer">
-          <img
-            className="carousel__image"
-            src="https://placekitten.com/320/210"
-            alt=""
-          />
-        </div>
-        <div className="carousel__imageContainer">
-          <img
-            className="carousel__image"
-            src="https://placekitten.com/319/210"
-            alt=""
-          />
-        </div>
+      <div
+        className="carousel__container"
+        style={{ transform: `translateX(-${activeItem * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="carousel__imageContainer">
+            <img className="carousel__image" src={image.src} alt="" />
+          </div>
+        ))}
       </div>
 
       <div className="carousel__slider">
-        <input
-          type="radio"
-          name="carousel"
-          className="carousel__radio"
-          checked
-        />
-        <input type="radio" name="carousel" className="carousel__radio" />
-        <input type="radio" name="carousel" className="carousel__radio" />
+        {images.map((_, index) => (
+          <input
+            key={index}
+            type="radio"
+            name="carousel"
+            className="carousel__radio"
+            checked={index === activeItem}
+            onChange={() => setactiveItem(index)}
+          />
+        ))}
       </div>
     </div>
   );
