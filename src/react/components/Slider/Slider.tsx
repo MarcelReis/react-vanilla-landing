4a16988fb58c9e4686b647969ad262e0f7a2e321
Marcelo Reis <oi@marcelreis.dev>
2020-12-06 23:29:23 -0300

    feat: react slider reimplementation

--- src/react/components/Slider/Slider.tsx
+++ src/react/components/Slider/Slider.tsx
@@ -1,172 +1,69 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+const items = [...new Array(9)];
+const gridGap = 20;
 
 const Slider = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+  const [activeIndex, setactiveIndex] = useState(0);
+
+  useEffect(() => {
+    const calcMobile = () => setIsMobile(window.innerWidth < 600);
+
+    window.addEventListener("resize", calcMobile);
+
+    return () => window.removeEventListener("resize", calcMobile);
+  }, []);
+
+  const backSlider = () => {
+    setactiveIndex(Math.max(activeIndex - (isMobile ? 1 : 3), 0));
+  };
+
+  const nextSlider = () => {
+    setactiveIndex(Math.min(activeIndex + (isMobile ? 1 : 3), 8));
+  };
+
   return (
     <section id="slider" className="section section--gray-2">
       <div className="slider__controlls">
-        <button className="slider__arrowBack">
+        <button className="slider__arrowBack" onClick={backSlider}>
           <svg className="icon">
             <use xlinkHref="#svg__arrowLeft" />
           </svg>
         </button>
 
         <div className="slider">
-          <div className="slider__container">
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
-
-            <figure className="slider__figure">
-              <img
-                src="https://placekitten.com/400/300"
-                alt=""
-                className="slider__image"
-              />
-              <figcaption className="slider__caption">
-                <p className="slider__title">Lorem ipsum</p>
-                <p className="slider__text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                  excepturi quas quidem voluptatum rerum qui quod. Quis, velit.
-                  Omnis qui officia optio veritatis unde ut rerum tenetur
-                  delectus numquam facere.
-                </p>
-              </figcaption>
-            </figure>
+          <div
+            className="slider__container"
+            style={{
+              transform: isMobile
+                ? `translateX(-${activeIndex * 100}%)`
+                : `translateX(calc(-${Math.floor(activeIndex / 3) * 100}% - ${
+                    Math.floor(activeIndex / 3) * gridGap
+                  }px))`,
+            }}
+          >
+            {items.map((_, index) => (
+              <figure key={index} className="slider__figure">
+                <img
+                  src="https://placekitten.com/400/300"
+                  alt=""
+                  className="slider__image"
+                />
+                <figcaption className="slider__caption">
+                  <p className="slider__title">Lorem ipsum</p>
+                  <p className="slider__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nihil excepturi quas quidem voluptatum rerum qui quod. Quis,
+                    velit. Omnis qui officia optio veritatis unde ut rerum
+                    tenetur delectus numquam facere.
+                  </p>
+                </figcaption>
+              </figure>
+            ))}
           </div>
         </div>
-        <button className="slider__arrowNext">
+        <button className="slider__arrowNext" onClick={nextSlider}>
           <svg className="icon">
             <use xlinkHref="#svg__arrowRight" />
           </svg>
